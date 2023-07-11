03bebbe581dc93bd98a2454710b6fef8a087a43f
Ben Leadholm <bclead3@yahoo.com>
2023-07-11 06:50:57 -0500

    Remove YUI from css_compressor in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,8 +30,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.css_compressor = :yui
-  # config.assets.js_compressor = :terser
+  # config.assets.css_compressor = :yui  # HEROKU can't use this
+  # config.assets.js_compressor = :terser # HEROKU can't use this
 
 
   # Enable serving of images, styles, and JavaScripts from an asset server.
